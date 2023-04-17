84d18c400d1a2715759bfd096ce7236102daedc3
Cussan03 <130979550+Cussan03@users.noreply.github.com>
2023-04-17 13:49:47 +0200

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Entrada {
     public static void main(String[] args) {
+        //Nueva version!!
         System.out.println("Computando");
         Salida.escribir();
         System.out.println("Fin de programa");
